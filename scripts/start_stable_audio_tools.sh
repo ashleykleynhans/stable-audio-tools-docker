942105a87c0b3b15fc5de6d11b398f9be475017a
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-06-10 22:18:37 +0200

    Fixed start script to load model correctly

--- scripts/start_stable_audio_tools.sh
+++ scripts/start_stable_audio_tools.sh
@@ -7,7 +7,6 @@ cd /workspace/stable-audio-tools
 echo "Starting stable-audio-tools"
 export GRADIO_SERVER_NAME="0.0.0.0"
 export GRADIO_SERVER_PORT="3001"
-nohup python3 run_gradio.py > /workspace/logs/stable-audio-tools.log 2>&1 &
 nohup python3 run_gradio.py \
     --model-config /workspace/stable-audio-tools/ckpt/model_config.json \
     --ckpt-path /workspace/stable-audio-tools/ckpt/model.safetensors > /workspace/logs/stable-audio-tools.log 2>&1 &
